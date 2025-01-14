347013ec228c3f33cd8e6a3acb5d1cba2eaaea2f
ajei1007 <yyurii.k.lee@gmail.com>
2025-01-14 17:27:04 +0100

    install tailwind css

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all JS, JSX, TS, and TSX files in the src folder
+    "./public/index.html",        // Include the public index.html
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
